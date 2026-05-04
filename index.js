93fd06b7f1838b0454461f6f422c1ae9a612a8df
Thanaa Khairy <thanaamater@gmail.com>
2026-05-04 19:50:06 +0300

    listen on some port

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use((err, req, res, next) => {
 module.exports = app;
 
 // // Start server
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
