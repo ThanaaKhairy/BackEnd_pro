a3d5c00aea37154355d932e427e8a6f65cabf30b
Thanaa Khairy <thanaamater@gmail.com>
2026-05-09 16:44:10 +0300

    last

--- index.js
+++ index.js
@@ -15,16 +15,19 @@ app.use(express.urlencoded({ extended: true }));
 // Routes
 app.use('/', authRoutes);
 
+
+
+
 // Error handler
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ error: 'Something went wrong!' });
 });
 
-module.exports = app;
+// module.exports = app;
 
 // // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
