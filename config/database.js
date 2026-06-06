ce5732f957a9c3ea43961431c90e1dcd648b48ff
Thanaa Khairy <thanaamater@gmail.com>
2026-06-06 13:48:14 +0300

    forcing db name

--- config/database.js
+++ config/database.js
@@ -7,16 +7,16 @@ if (!cached) {
 }
 
 async function connectDB() {
-  // if (cached.conn) {
-  //   return cached.conn;
-  // }
+  if (cached.conn) {
+    return cached.conn;
+  }
 
-  // if (!cached.promise) {
+  if (!cached.promise) {
     cached.promise = mongoose.connect(process.env.MONGODB_URI,{dbName:'Digi_GP'}).then((mongoose) => {
       console.log('MongoDB Connected');
       return mongoose;
     });
-  // }
+  }
   
   cached.conn = await cached.promise;
   return cached.conn;
